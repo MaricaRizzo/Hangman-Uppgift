7fcc472ac97c5ea457195fb09c4bdbef9b3c120b
MaricaRizzo <maricarizzoo@gmail.com>
2020-11-03 14:42:47 +0100

    Basic javascript update

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ let wordStatus = null;
 
 
 
-// Give random word from list of words
+// Start game on click
 document.getElementById('startBtn').addEventListener('click', () =>{
     document.getElementById('startBtn').classList.toggle('hide');
     document.getElementById('gameContainer').classList.toggle('hide');
 })
 
+
+// Give random word from list of words
 function getWord() {
-    answer = randomWords[Math.floor(Math.random()*randomWords.length)];
-    
+    answer = randomWords[Math.floor(Math.random()*randomWords.length)]; 
 }  
 
 
 
-// Split guess word and create guess word empty cells 
+// Split guess word 
+// Fill in word with guessed letter or
+// Create empty cells 
 function guessedWord() {
     wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : '_')).join(' ');
+
     document.getElementById('word').innerHTML = wordStatus;
 }
 
@@ -41,6 +45,7 @@ function generateButtons() {
         ` + letter + `
         </button>
         `).join('');
+
         document.getElementById('alphabet').innerHTML = buttonsHTML;
 }
 
@@ -53,7 +58,9 @@ function handleGuess(chosenLetter) {
     } else {
         null;
     } 
+
     document.getElementById(chosenLetter).setAttribute('disabled', true);
+
 
     if (answer.indexOf(chosenLetter) >= 0) {
         guessedWord();
@@ -76,7 +83,7 @@ document.getElementById('maxWrong').innerHTML = maxWrong;
 
 
 
-// Check for game result
+// Check for game result ---> not working
 function checkIfGameWon() {
     if (wordStatus === answer) {
         document.getElementById('alphabet').innerHTML = 'You won!'
